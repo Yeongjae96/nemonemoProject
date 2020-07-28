2c453591943fc7eb45d0820c8b0ba3ab506b2f96
Yeong <enoughng@naver.com>
2020-07-28 22:48:59 +0900

    InsertBoard의 대략적인 구조 및 products/new.do 에 대한 스크립트
    *****************************************************
    - 진행중
    * 사진 등록시 미리보기 이미지 생성(8개까지 가능)
    *  거래지역 - 내위치 버튼시 현재 내 위치 불러오는 카카오API 사용
    * 가격 입력시 숫자 입력 확인 스크립트 
    * 연관태그입력 후 엔터 시 li들 생성
    
    *****************************************************
    
    - 앞으로 해야할 것
    * 사진 등록에 대한 사진 정보 저장할 배열같은 것
    * 제목 입력 시 최대글자 유효성 검사
    * 카테고리 데이터 바인딩
    * 거래지역의 내지역외의 버튼 이벤트(최근지역, 지하철 검색)
    * 설명에 대한 최대글자 유효성 검사
    * 연관태그 입력 후 엔터 이벤트 시 2개의 버튼에 대한 이벤트
    *****************************************************
    - 논의 할 점
    * 등록에 대해 서버에게 어떻게 넘겨줄지 결정(폼 데이터)

---
+++ nemonemoProject/src/main/java/com/nemo/user/products/vo/ProductsVO.java
@@ -0,0 +1,19 @@
+package com.nemo.user.products.vo;
+
+/**
+ * @제목 : 상품 정보 VO
+ * @패키지이름 : com.nemo.user.products.vo
+ * @파일이름 : ProductsVO.java
+ * @작성일 : 2020. 7. 27.
+ * @이름 : Yeong
+ * @프로그램설명 : Products 테이블과 매핑되는 상품 VO 클래스
+ * == 수정 정보 ==
+ *
+ * DATE			 AUTHOR			NOTE
+ * -------		--------			-------------	
+ * 20.07.27 	  김영재				테이블 명세서에 따른 초기세팅
+ */
+
+public class ProductsVO {
+	
+}
