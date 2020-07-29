b53f29ce8987b4e26a98d8ac935c5735b9e75453
Yeong <enoughng@naver.com>
2020-07-29 13:11:47 +0900

    Notice 컨트롤러 빠진 주석 추가
    * Notice 컨트롤러 주석 추가

--- nemonemoProject/src/main/java/com/nemo/admin/sites/controller/NoticeController.java
+++ nemonemoProject/src/main/java/com/nemo/admin/sites/controller/NoticeController.java
@@ -5,6 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * @제목 : 공지사항 컨트롤러
+ * @패키지이름 : com.nemo.admin.sites.controller
+ * @파일이름 : NoticeController.java
+ * @작성일 : 2020. 7. 29.
+ * @이름 : Yeong
+ * @프로그램설명 : 관리자 공지사항 페이지의 경로를 잡아주는 컨트롤러
+ * == 수정 정보 ==
+ *
+ * DATE				 AUTHOR			NOTE
+ * -------			  --------		-------------	
+ * 20. 07.29 			김영재				초기설정
+ */
 @Controller
 public class NoticeController {
 	
