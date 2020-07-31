4c72f7ba234a30d44a4effe89eeb708f5f587ad4
Yeong <enoughng@naver.com>
2020-07-31 12:58:45 +0900

    회원가입 페이지 연결
    * 회원가입 페이지 연결

--- nemonemoProject/src/main/java/com/nemo/admin/management/controller/ManagementController.java
+++ nemonemoProject/src/main/java/com/nemo/admin/management/controller/ManagementController.java
@@ -2,6 +2,7 @@ package com.nemo.admin.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -54,8 +55,6 @@ public class ManagementController {
 		insertAdminService.insertAdmin(vo);
 		ModelAndView mav = new ModelAndView("redirect:/management/signin.mdo");
 		return mav;
-
-
 	}
 
 
