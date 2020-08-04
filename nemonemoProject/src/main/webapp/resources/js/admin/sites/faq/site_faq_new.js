18a2b1cfe24dd83f57786db75e6e232b048cbda0
Yeongjae96 <yeongjae96@gmail.com>
2020-08-05 01:45:42 +0900

    site_faq_new.js 커밋내용 누락 추가
    site_faq_new.js 커밋내용 누락 추가

--- nemonemoProject/src/main/webapp/resources/js/admin/sites/faq/site_faq_new.js
+++ nemonemoProject/src/main/webapp/resources/js/admin/sites/faq/site_faq_new.js
@@ -6,13 +6,14 @@ $(function() {
 
 /* FAQ 카테고리 불러오기 */
 function getFaqCategory() {
+	/* AJAX 요청 */
 	$.ajax({
-		url: 'category/listJson.mdo',
-		method: 'GET',
-		dataType: 'json',
+		url: 'category/listJson.mdo', // 요청 URL
+		method: 'GET', // 요청 방식
+		dataType: 'json', // ㅇ
 	}).done(function(data) {
 		$.each(data, function(i, e) {
-			$option = $('<option></option').text(e.faqCategoryName);
+			$option = $('<option></option').attr('value', e.faqCategoryNo).text(e.faqCategoryName);
 			$('.faq-category--list').append($option);
 		});
 	}).fail(function(data) {
@@ -21,9 +22,38 @@ function getFaqCategory() {
 	
 };
 
-
+/* 제목 size check */
 function sizeCheck(textId, sizeId, size) {
-	(`#${textId}`).on('keyup', function() {
-		(`#${sizeId} span`).text($(this).val());
+	
+	const $textId = $(`#${textId}`);
+	const $sizeId = $(`#${sizeId}`);
+	
+	$textId.on('keyup', function() {
+		const length = $textId.val().length;
+		if(length > 30) {
+			alert('30자를 초과할 수 없습니다.');
+			$textId.val($textId.val().substring(0,30));
+			return false;
+		}
+		
+		$sizeId.text(length);
 	});
 }
+
+/* 텍스트 에디터로 인해 만들다 말음 
+function newBtnClickSetEvent(btnId) {
+	const $btnId = $(`#${btnId}`);
+	$btnId.click(function() {
+		const param = {
+			faqCategoryNo: $('.faq-category--list option:selected').val(),
+			faqTitle: $('#inputTitle'),
+			faqContent
+		};
+		$.ajax({
+			url: 'new.do',
+			method: 'post',
+			data: 
+		});
+	});
+}*/
+
