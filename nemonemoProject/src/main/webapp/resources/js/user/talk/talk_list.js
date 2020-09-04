b785876b24629dec826ac2b7ac22f5d26e4d86d6
Yeongjae96 <yeongjae96@gmail.com>
2020-09-04 13:06:06 +0900

    chat UI 구성중
    chat UI 구성중

--- nemonemoProject/src/main/webapp/resources/js/user/talk/talk_list.js
+++ nemonemoProject/src/main/webapp/resources/js/user/talk/talk_list.js
@@ -3,31 +3,38 @@ $(function() {
 });
 
 function openSocket() {
-	const wsUri = contextPath + "talk/open.do";
+	const wsUri = "ws://localhost"+contextPath + "talk/open.do";
 	const webSocket = new WebSocket(wsUri);
 	webSocket.onopen = onOpen;
 	webSocket.onmessage = onMessage;
 	webSocket.onerror = onError;
-}
-
-/* 소켓이 열렸을 때 */
-function opOpen(evt) {
-	alert('open : ', evt.data);
-	sendMessage('열렸습니다.');
-}
-
-function onMessage(evt) {
-	alert('수신 : ', evt.data);
 	
+	/* 소켓이 열렸을 때 */
+	function onOpen(evt) {
+		console.log(evt); 
+		sendMessage('열렸습니다.');
+	}
+
+	/* 서버로 부터 수신 */
+	function onMessage(evt) {
+		console.log(evt);
+	}
+
+	/* 에러 발생 시 */
+	function onError(evt) {
+		console.log(evt);
+	}
+
+	/* 내가 정의 하는 함수 : 서버로 보내는것  */
+	function sendMessage(obj) {
+		
+		if(obj.constructor !== object) {
+			obj = new Object();
+			obj.msgContent = '';
+		}
+		webSocket.send(msgContent);
+	}
 }
 
-function onError(evt) {
-	alert('에러 발생  :', evt.data);
-	
-}
 
-
-function sendMessage(text) {
-	webSocket.send(text);
-}
 
